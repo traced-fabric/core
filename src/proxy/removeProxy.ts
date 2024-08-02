e87a57156f75865dd9911604090120881be8e7b8
andrey kovyarov <akuzmenko@cloverdynamics.com>
2024-08-02 16:13:53 +0300

    remove proxy now clones structures to remove proxy

--- src/proxy/removeProxy.ts
+++ src/proxy/removeProxy.ts
@@ -1,14 +1,13 @@
 import { symbolTracedDataIndication } from '../utils/tracedDataSymbol';
 
 export function removeProxy(value: any): any {
-  if (value !== null && typeof value === 'object' && value[symbolTracedDataIndication]) {
-    let copiedValue: any;
+  const copiedValue = structuredClone(value);
 
-    if (Array.isArray(value)) copiedValue = [...value];
-    else copiedValue = { ...value };
-
-    if (copiedValue[symbolTracedDataIndication]) delete copiedValue[symbolTracedDataIndication];
-    return copiedValue;
+  if (
+    value !== null && typeof value === 'object'
+    && copiedValue[symbolTracedDataIndication]
+  ) {
+    delete copiedValue[symbolTracedDataIndication];
   }
 
   return value;
