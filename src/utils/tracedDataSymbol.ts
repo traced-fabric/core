b162d7133eae6dc493fe5638118c646f1f39055f
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-04 21:35:35 +0300

    refactored symbols

--- src/utils/tracedDataSymbol.ts
+++
@@ -1,4 +0,0 @@
-export const symbolTracedFabric = Symbol('tracedFabric');
-
-export const symbolTracedDataId = Symbol('tracedDataId');
-export const symbolTracedDataIndication = Symbol('tracedDataIndication');
