a47f43a3f9c4b4031b020448c5f0ed36e1f66f9d
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-05 18:14:32 +0300

    added jsDoc

--- src/deepClone.ts
+++ src/deepClone.ts
@@ -1,8 +1,25 @@
+// For tracedFabric structuredClone can't be used,
+// because it breaks when trying to copy traced values and general slowness.
+
 /**
- * Deep clone an object or array
- * For tracedFabric structuredClone can't be used, because it breaks when trying to copy traced values and general slowness
+ * Deep clone an object or array.
+ * Primarily used to clone traceFabric values without inheriting proxy metadata,
+ * and potentially breaking tracedFabric.
  *
  * @note The deepClone will not copy symbols
+ *
+ * @param value - The value to clone
+ * @returns The cloned value
+ *
+ * @example
+ * const fabric = traceFabric({ season: 'winter', bestDays: [2, 7, 16] });
+ * const clone = deepClone(origin.value);
+ *
+ * console.log(clone); // { season: 'winter', bestDays: [2, 7, 16] }
+ * console.log(clone === origin); // false
+ * console.log(clone.bestDays === origin.bestDays); // false
+ *
+ * @since 0.0.1
  */
 export function deepClone<T>(value: T): T {
   if (typeof value !== 'object') return value;
--- src/traceFabric.ts
+++ src/traceFabric.ts
@@ -24,6 +24,28 @@ export type TTracedFabric<T extends JSONStructure> = {
   clearTrace: () => void;
 };
 
+/**
+ * Trace a JSON object or array to track mutations.
+ * Other tracedFabric objects can be used as children.
+ *
+ * The traceChanges, that are produced by the tracedFabric when mutation values,
+ * can be used to apply the same mutations to other objects or arrays.
+ * This is needed primarily for sharing the same object state between different environments,
+ * where simple object references can't be used. (e.g. Workers, WebSockets, iframes, etc.)
+ *
+ * @param value JS object or array that resembles a JSON structure
+ * @returns The tracedFabric object
+ *
+ * @example
+ * const fabric = traceFabric({ season: 'winter', bestDays: [2, 7, 16] });
+ *
+ * fabric.value.season = 'summer';
+ * fabric.value.bestDays.push(25);
+ *
+ * console.log(fabric.getTrace());
+ *
+ * @since 0.0.1
+ */
 export function traceFabric<T extends JSONStructure>(value: T): TTracedFabric<T> {
   const id = getTracedValueId();
 
