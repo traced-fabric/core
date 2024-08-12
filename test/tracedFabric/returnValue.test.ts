f024df73d4ce5c2ecf00a7636a9e9f26e36aeff0
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-12 18:46:08 +0300

    add tests for tracedFabric's return value

---
+++ test/tracedFabric/returnValue.test.ts
@@ -0,0 +1,75 @@
+import { describe, expect, test } from 'bun:test';
+import { traceFabric } from '../../src/traceFabric';
+import { EMutated, EObjectMutation } from '../../src/types/mutation';
+import { symbolTracedFabric } from '../../src/utils/symbols';
+import { deepClone } from '../../src/deepClone';
+
+describe('tracedFabric should return ', () => {
+  test('should have a tracedFabric symbol', () => {
+    const tracing = traceFabric({ string: 'string' });
+
+    expect(tracing[symbolTracedFabric]).toEqual(true);
+  });
+
+  test('to have proxy value', () => {
+    const tracing = traceFabric({ string: 'string' });
+
+    expect(deepClone(tracing.value)).toEqual({ string: 'string' });
+  });
+
+  test('function to clear trace', () => {
+    const tracing = traceFabric({ string: 'string' });
+
+    tracing.value.string = 'new string';
+
+    expect(tracing.getTrace()).toEqual([{
+      mutated: EMutated.object,
+      type: EObjectMutation.set,
+      targetChain: ['string'],
+      value: 'new string',
+    }]);
+
+    tracing.clearTrace();
+
+    expect(tracing.getTrace()).toEqual([]);
+    expect(tracing.getTraceLength()).toEqual(0);
+  });
+
+  test('function to get trace length', () => {
+    const tracing = traceFabric({ string: 'string' });
+
+    tracing.value.string = 'new string';
+    tracing.value.string = 'new string 1';
+    tracing.value.string = 'new string 2';
+    tracing.value.string = 'new string 3';
+
+    expect(tracing.getTraceLength()).toEqual(4);
+  });
+
+  test('function to get trace', () => {
+    const tracing = traceFabric({ string: 'string' });
+
+    tracing.value.string = 'new string';
+
+    expect(tracing.getTrace()).toEqual([{
+      mutated: EMutated.object,
+      type: EObjectMutation.set,
+      targetChain: ['string'],
+      value: 'new string',
+    }]);
+
+    tracing.value.string = 'new string 2';
+
+    expect(tracing.getTrace()).toEqual([{
+      mutated: EMutated.object,
+      type: EObjectMutation.set,
+      targetChain: ['string'],
+      value: 'new string',
+    }, {
+      mutated: EMutated.object,
+      type: EObjectMutation.set,
+      targetChain: ['string'],
+      value: 'new string 2',
+    }]);
+  });
+});
