ebe42fb70b86714c768b0732ecfe0217e17f0c19
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-13 02:10:04 +0300

    remove getTracedValue from index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 export * from './src/proxy/deepTrace';
 export * from './src/proxy/getTracedArray';
 export * from './src/proxy/getTracedObject';
-export * from './src/proxy/getTracedValue';
 
 export * from './src/types/json';
 export * from './src/types/mutation';
