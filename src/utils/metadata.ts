79ecdc8f8f1c532bc7568145f4e045f4c4b38b61
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-13 23:23:03 +0300

    added ability to getTraceChain by metadata

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,9 +1,17 @@
-import type { JSONArray, JSONStructure } from '../types/json';
-import type { TRequiredApplyProxyParams } from '../types/mutation';
+import type { JSONStructure } from '../types/json';
+import type { TTarget } from '../types/mutation';
 
-export type TTracedValueMetadata = Pick<
-  TRequiredApplyProxyParams<JSONArray>,
-  'targetChain'
->;
+export type TTracedValueMetadata = {
+  parentRef: JSONStructure;
+  key: TTarget;
+};
 
 export const tracedValuesMetadata = new WeakMap<JSONStructure, TTracedValueMetadata>();
+
+export function getTargetChain(target: JSONStructure): TTarget[] {
+  const metadata = tracedValuesMetadata.get(target);
+
+  if (!metadata) return [];
+
+  return [...getTargetChain(metadata.parentRef), metadata.key];
+}
