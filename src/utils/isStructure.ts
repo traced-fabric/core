5c706b4ea543338748aaad12cd3845d244b4444d
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-24 23:34:13 +0300

    added JSDoc to isStructure function

--- src/utils/isStructure.ts
+++ src/utils/isStructure.ts
@@ -1,3 +1,11 @@
+/**
+ * Check if the value is a `typeof 'object'` and not `null`.
+ *
+ * @param value - value to check
+ * @returns true if the value is a structure, false otherwise
+ *
+ * @since 0.2.0
+ */
 export function isStructure<T>(value: T): boolean {
   return typeof value === 'object' && value !== null;
 }
