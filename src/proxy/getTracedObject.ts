d5f23c2e4db81cd5cf5c201c2264bd214cb1acce
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-24 15:08:54 +0300

    refactored getTracedObject to follow getTracedArray code

--- src/proxy/getTracedObject.ts
+++ src/proxy/getTracedObject.ts
@@ -1,56 +1,72 @@
 import type { JSONObject } from '../types/json';
-import { EMutated, EObjectMutation, type TRequiredApplyProxyParams } from '../types/mutation';
-import { tracedSubscribers, tracedValues } from '../utils/references';
+import { EMutated, EObjectMutation, type TMutationCallback } from '../types/mutation';
 import { deepClone } from '../deepClone';
+import { isTracing } from '../utils/withoutTracing';
+import { type TTracedValueMetadata, getTargetChain } from '../core/metadata';
+import { addTracedSubscriber, isTracedValue, removeTracedSubscriber } from '../core/references';
+import type { TTracedFabricValue } from '../types/tracedValue';
 import { deepTrace } from './deepTrace';
 
-export function getTracedProxyObject<T extends JSONObject>(data: TRequiredApplyProxyParams<T>): T {
-  const mutatedType = EMutated.object;
+export function getTracedProxyObject<T extends JSONObject>(
+  value: T,
+  mutationCallback: TMutationCallback,
+  metadata?: TTracedValueMetadata,
+): T {
+  const mutated = EMutated.object;
 
-  return new Proxy(data.value, {
-    set(target, key, value) {
+  return new Proxy(value, {
+    set(target, key, value, receiver) {
       if (typeof key === 'symbol') return Reflect.set(target, key, value);
 
-      const targetChain = [...data.targetChain, key];
+      const targetChain = [...getTargetChain(receiver), key];
 
-      if (tracedValues.has(target[key] as any)) {
-        const subscribers = tracedSubscribers.get(target[key] as any)![data.originId];
-        if (subscribers) delete subscribers[targetChain.join('')];
+      // if the value that is overridden and it is a tracedFabric,
+      // we should remove the subscriber from the old value
+      if (isTracedValue(target[key])) {
+        removeTracedSubscriber(target[key], {
+          rootRef: metadata?.rootRef ?? receiver,
+          parentRef: receiver,
+          key,
+        });
       }
 
-      data.mutationCallback({
-        mutated: mutatedType,
-        value: deepClone(value),
-        targetChain,
-        type: EObjectMutation.set,
-      });
-
-      if (tracedValues.has(value)) {
-        data.onCaughtTrace({ subscriber: value, targetChain });
-        return Reflect.set(target, key, value);
+      // if new value is tracedFabric, we should subscribe the current
+      // tracedFabric to the new value mutations
+      if (isTracedValue(value)) {
+        addTracedSubscriber(value, {
+          rootRef: metadata?.rootRef ?? receiver,
+          parentRef: receiver,
+          key,
+        });
       }
 
-      const proxyValue = deepTrace({ ...data, targetChain, value }).proxy;
+      if (isTracing()) mutationCallback({ mutated, targetChain, value: deepClone(value), type: EObjectMutation.set });
 
-      return Reflect.set(target, key, proxyValue);
+      const proxy = deepTrace(value, mutationCallback, metadata
+        ? {
+            rootRef: metadata.rootRef,
+            parentRef: receiver,
+            key,
+          }
+        : undefined);
+
+      return Reflect.set(target, key, proxy);
     },
+
     deleteProperty(target, key) {
-      if (typeof key === 'symbol') return Reflect.deleteProperty(target, key);
+      if (typeof key === 'symbol') return Reflect.set(target, key, value);
 
-      const subscribers = tracedSubscribers.get(target[key] as any)?.[data.originId];
+      const targetChain = [...getTargetChain(target), key];
 
-      if (
-        subscribers
-        && tracedValues.has(target[key] as any)
-      ) {
-        delete subscribers[data.targetChain.concat(key).join('')];
+      if (isTracedValue(target[key])) {
+        removeTracedSubscriber(target[key], {
+          rootRef: metadata?.rootRef ?? target as unknown as TTracedFabricValue,
+          parentRef: target,
+          key,
+        });
       }
 
-      data.mutationCallback({
-        mutated: mutatedType,
-        targetChain: [...data.targetChain, key],
-        type: EObjectMutation.delete,
-      });
+      if (isTracing()) mutationCallback({ mutated, targetChain, type: EObjectMutation.delete });
 
       return Reflect.deleteProperty(target, key);
     },
