4675dc1df3afc32c2502f4d2dcbd1def726feb56
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-28 16:43:37 +0300

    added TTraceFabricOptions type mutationMap function definition

--- src/types/tracedFabric.ts
+++ src/types/tracedFabric.ts
@@ -1,11 +1,30 @@
 import type { JSONStructure } from './json';
 import type { TTraceChange } from './mutation';
 
-export type TTracedFabric<T extends JSONStructure> = {
-  // The proxy object that is used to trace the mutations
+export type TTraceFabricOptionUnknown = Partial<Record<keyof TTraceFabricOptionDefaults, unknown>>;
+export type TTraceFabricOptionDefaults = {
+  TraceChange: TTraceChange;
+};
+
+export type TTracedFabricMutationMap<T = TTraceChange> = (mutation: TTraceChange) => T;
+
+export type TTraceFabricOptions<_OPTIONS extends TTraceFabricOptionUnknown = TTraceFabricOptionDefaults> = {
+  mutationMap: TTracedFabricMutationMap<_OPTIONS['TraceChange']>;
+};
+
+/**
+ * The output of the traceFabric function
+ *
+ * @param T - The type of the original object, which will be traced
+ * @param _OPTIONS - The options for the traceFabric (TTraceFabricOptions)
+ */
+export type TTracedFabric<
+  T extends JSONStructure,
+  _OPTIONS extends TTraceFabricOptionUnknown = TTraceFabricOptionDefaults,
+> = {
   value: T;
 
-  getTrace: () => TTraceChange[];
+  getTrace: () => _OPTIONS['TraceChange'][];
   getTraceLength: () => number;
 
   clearTrace: () => void;
