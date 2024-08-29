6cd6e7d9204714442ec985ae2347a43cd1e1f99d
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-08-29 18:53:52 +0300

    added tests for exported types and functions

---
+++ test/exports.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from 'bun:test';
+import * as TF from '../index';
+
+const expectType = <T>(_?: T) => ({});
+
+describe('index file should export', () => {
+  test('removeTraceSubscription', () => expect(TF.removeTraceSubscription).toBeDefined());
+
+  test('types json', () => {
+    expectType<TF.JSONPrimitive>();
+    expectType<TF.JSONObject>();
+    expectType<TF.JSONArray>();
+    expectType<TF.JSONStructure>();
+    expectType<TF.JSONValue>();
+  });
+
+  test('types mutation', () => {
+    expect(TF.EObjectMutation).toBeDefined();
+    expect(TF.EArrayMutation).toBeDefined();
+    expect(TF.EMutated).toBeDefined();
+    expectType<TF.TTarget>();
+    expectType<TF.TObjectMutation>();
+    expectType<TF.TArrayMutation>();
+    expectType<TF.TTraceChange>();
+    expectType<TF.TMutationCallback>();
+  });
+
+  test('isStructure', () => expect(TF.isStructure).toBeDefined());
+  test('isTraced', () => expect(TF.isTraced).toBeDefined());
+  test('withoutTracing', () => expect(TF.withoutTracing).toBeDefined());
+
+  test('applyTrace', () => expect(TF.applyTrace).toBeDefined());
+  test('deepClone', () => expect(TF.deepClone).toBeDefined());
+  test('traceFabric', () => expect(TF.traceFabric).toBeDefined());
+});
