3b800cc3c35a5ac9b9ca5a9f229fb85f73b8bca3
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-01 18:27:18 +0300

    added better type support for isStructure

--- src/utils/isStructure.ts
+++ src/utils/isStructure.ts
@@ -1,3 +1,5 @@
+import type { JSONObject } from '../types/json';
+
 /**
  * Check if the value is a `typeof 'object'` and not `null`.
  *
@@ -6,6 +8,8 @@
  *
  * @since 0.2.0
  */
-export function isStructure<T>(value: T): boolean {
+export function isStructure<T>(
+  value: T,
+): value is T extends JSONObject ? T : T & JSONObject {
   return typeof value === 'object' && value !== null;
 }
