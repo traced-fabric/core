6ce5670cf461dfe9606039731adcb5845b250389
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-01 18:26:05 +0300

    added array push mutation definition

--- src/types/mutation.ts
+++ src/types/mutation.ts
@@ -1,22 +1,16 @@
 import type { JSONValue } from './json';
 
-export enum EObjectMutation {
-  set = 'set',
-  delete = 'delete',
-}
-
-export enum EArrayMutation {
-  set = 'set',
-  delete = 'delete',
-  reverse = 'reverse',
-}
-
 export enum EMutated {
   object = 'object',
   array = 'array',
 }
 
 export type TTarget = string | number;
+
+export enum EObjectMutation {
+  set = 'set',
+  delete = 'delete',
+}
 
 export type TObjectMutation = {
   mutated: EMutated.object;
@@ -29,6 +23,14 @@ export type TObjectMutation = {
   value?: never;
 });
 
+export enum EArrayMutation {
+  set = 'set',
+  delete = 'delete',
+
+  push = 'push',
+  reverse = 'reverse',
+}
+
 export type TArrayMutation = {
   mutated: EMutated.array;
   targetChain: TTarget[];
@@ -38,6 +40,9 @@ export type TArrayMutation = {
 } | {
   type: EArrayMutation.delete;
   value?: never;
+} | {
+  type: EArrayMutation.push;
+  value: JSONValue[];
 } | {
   type: EArrayMutation.reverse;
   value?: never;
