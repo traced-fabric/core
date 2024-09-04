14123757f82869b2a024912df4c945d779726e19
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-04 14:34:23 +0300

    updated JSDoc of isTracing and withoutTracing functions

--- src/utils/withoutTracing.ts
+++ src/utils/withoutTracing.ts
@@ -1,10 +1,20 @@
 let allowTracing = true;
 
 /**
- * Checks if tracing is enabled.
- * If the tracing is enabled, the mutations are recorded in the traceLogs.
+ * Check if `tracing` is enabled. If the tracing is enabled, the `mutations` are recorded in the trace.
  *
- * @returns true if tracing is enabled, false otherwise
+ * @returns true if `tracing` is enabled, false otherwise
+ *
+ * @example
+ * ```typescript
+ * console.log(isTracing()); // true
+ *
+ * withoutTracing(() => {
+ *   console.log(isTracing()); // false
+ * });
+ * ```
+ *
+ * @see {@link https://github.com/traced-fabric/core/wiki/%F0%9F%A7%B0-Essentials-%7C-Package-exports#-istracing Wiki page.}
  *
  * @since 0.2.0
  */
@@ -13,13 +23,13 @@ export function isTracing(): boolean {
 }
 
 /**
- * Ignores all mutations recording to traceLogs in the given function.
+ * Ignores recording of all `mutations` to `trace` in the given function.
  * The function should not be async, as it turns off the tracing globally.
  *
- * @warn use with caution, as it can lead to breaking applyTrace function, because of the missing mutations.
+ * @warn Use with caution, as it can lead to breaking `applyTrace` function, because of the missing mutations.
  *
- * @param callback - function in which tracing is disabled
- * @returns result of the callback
+ * @param callback - function in which `tracing` is disabled
+ * @returns same as the return value of the given function
  *
  * @example
  * const fabric = traceFabric({ season: 'winter' });
