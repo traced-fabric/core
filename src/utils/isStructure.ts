c9afd515e258a663fc29625bdd093037f1b134ce
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-04 14:19:17 +0300

    update isStructure jsDoc

--- src/utils/isStructure.ts
+++ src/utils/isStructure.ts
@@ -3,7 +3,7 @@ import type { JSONObject } from '../types/json';
 /**
  * Check if the value is a typeof object and not null.
  *
- * @param value - value that needs to be checked
+ * @param value - value that needs to be checked.
  * @returns `boolean`
  *
  * @example
@@ -19,7 +19,7 @@ import type { JSONObject } from '../types/json';
  * console.log(isStructure(true)); // false
  * ```
  *
- * @see {@link https://github.com/traced-fabric/core/wiki/%F0%9F%A7%B0-Essentials-%7C-Package-exports#-isstructure Wiki page}.
+ * @see {@link https://github.com/traced-fabric/core/wiki/%F0%9F%A7%B0-Essentials-%7C-Package-exports#-isstructure Wiki page.}
  *
  * @since 0.2.0
  */
