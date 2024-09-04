0ea19badc547d9a7e0016ed5c0c86af6218c0a91
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-04 14:53:25 +0300

    updated traceFabric JSDoc

--- src/traceFabric.ts
+++ src/traceFabric.ts
@@ -9,15 +9,20 @@ import { tracedFabricsTrace } from './core/traces';
 
 /**
  * Track the mutation of a given JSON-like object or array.
- * Other tracedFabric objects can be used as children.
+ * Other `tracedFabric` can be nested inside.
  *
- * The traceChanges *(mutations)*, that are produced by the tracedFabric on values mutation,
- * can be used to apply them to other objects or arrays.
- * This is needed primarily for sharing the same object state between different environments,
- * where simple object references can't be used. (e.g. Workers, WebSockets, iframes, etc.)
+ * The `trace` (array of `mutations`), that is produced by the `tracedFabric` on values mutation,
+ * can be used to apply them to other objects or arrays, using the `applyTrace(...)` function.
  *
- * @param value JS object or array that resembles a JSON structure
- * @returns The tracedFabric object
+ * @see {@link https://github.com/traced-fabric/core/wiki/%F0%9F%A7%B0-Essentials-%7C-Package-exports#-tracefabric Wiki page.}
+ *
+ * @param {JSONStructure} value an object or array that will be deeply tracked. Value is JSON.stringify safe. The type of the value should be the same as JSONStructure.
+ * @param {TOnMutation} [onMutation] a function that is triggered when the `traced` value is mutated. The function receives the `mutation` as an argument and should return the `mutation` to be saved to the `trace`. If no modification is needed, return the original mutation; otherwise, return the modified mutation for storage.
+ *
+ * @returns {TTracedFabric} Object with the following properties:
+ * **value** - the `traced` proxy of the given **value**.
+ * **trace** - the array of `mutations` that are made to the **value**.
+ * **clearTrace** - a function that clears the trace.
  *
  * @example
  * const bestDays = traceFabric([2, 7, 16]);
