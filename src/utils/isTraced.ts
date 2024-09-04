62c39faa69d44ec5d74bd7d7f3f8593bfcc04370
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-04 14:39:09 +0300

    added wiki page to isTraced.. functions

--- src/utils/isTraced.ts
+++ src/utils/isTraced.ts
@@ -7,12 +7,16 @@ import { tracedFabricsTrace } from '../core/traces';
  * To check the definition of `tracedFabric`, see [Wiki - 📜 Naming](https://github.com/traced-fabric/core/wiki/%F0%9F%93%9C-Naming)
  *
  * @example
+ * ```typescript
  * const traced = traceFabric({ // --> traced root value AND traced
  *   innerArray: [1, 2, 3], // --> traced value AND traced
  * });
  *
  * isTracedRootValue(traced.value); // true
  * isTracedRootValue(traced.value.innerArray); // false
+ * ```
+ *
+ * @see {@link https://github.com/traced-fabric/core/wiki/%F0%9F%A7%B0-Essentials-%7C-Package-exports#-istracedfabric Wiki page.}
  *
  * @since 0.2.0
  */
@@ -26,12 +30,16 @@ export function isTracedRootValue(value: any): boolean {
  * To check the definition of `tracedValue`, see [Wiki - 📜 Naming](https://github.com/traced-fabric/core/wiki/%F0%9F%93%9C-Naming)
  *
  * @example
+ * ```typescript
  * const traced = traceFabric({ // --> traced root value AND traced
  *   innerArray: [1, 2, 3], // --> traced value AND traced
  * });
  *
  * isTracedValue(traced.value); // false
  * isTracedValue(traced.value.innerArray); // true
+ * ```
+ *
+ * @see {@link https://github.com/traced-fabric/core/wiki/%F0%9F%A7%B0-Essentials-%7C-Package-exports#-istracedvalue Wiki page.}
  *
  * @since 0.2.0
  */
@@ -45,12 +53,16 @@ export function isTracedValue(value: any): boolean {
  * To check the definition of `traced`, see [Wiki - 📜 Naming](https://github.com/traced-fabric/core/wiki/%F0%9F%93%9C-Naming)
  *
  * @example
+ * ```typescript
  * const traced = traceFabric({ // --> traced root value AND traced
  *   innerArray: [1, 2, 3], // --> traced value AND traced
  * });
  *
  * isTraced(traced.value); // true
  * isTraced(traced.value.innerArray); // true
+ * ```
+ *
+ * @see {@link https://github.com/traced-fabric/core/wiki/%F0%9F%A7%B0-Essentials-%7C-Package-exports#-istraced Wiki page.}
  *
  * @since 0.2.0
  */
