9ffc2f89ab848ebd50da6be4c39ea4d400710f09
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-15 15:47:51 +0300

    refactor navigation for vitepress, update guide link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,8 +14,7 @@ export default defineConfig({
 
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'Guide', link: '/getting-started/introduction' },
     ],
 
     sidebar: [
