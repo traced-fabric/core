205510c43c87a2af691f43a32c5cc64f655bbc3c
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-21 16:43:35 +0300

    added demoContainer component

---
+++ docs/.vitepress/components/DemoContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>Demo</h2>
+
+  <section class="demo">
+    <slot />
+  </section>
+</template>
+
+<style lang="scss">
+.demo {
+  padding: var(--docsearch-spacing);
+  border: 2px solid var(--vp-c-default-1);
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+</style>
