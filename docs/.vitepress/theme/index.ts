419e689f7e13702f181e7f9bdd4445131b369a53
Andrew <andrey.kovyarov@gmail.com>
2024-09-25 22:29:36 +0300

    added tailwind css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 // https://vitepress.dev/guide/custom-theme
 import './style.css';
+import './tailwind.css';
 
 export default {
   extends: DefaultTheme,
