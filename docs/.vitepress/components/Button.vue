406bced6334449a8fad5da43469e13312eaf30f0
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-26 15:53:26 +0300

    added custom button

---
+++ docs/.vitepress/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="px-2 py-1 rounded text-[var(--vp-code-color)] transition-colors hover:text-[var(--vp-c-text-1)] bg-[var(--vp-code-bg)]">
+    <slot />
+  </button>
+</template>
